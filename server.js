5e39563a33f98afd182b43a2699b66f83f14dab1
alexm <alex-m@outlook.com>
2023-05-02 18:24:21 -0700

    Put hostname and port in variables; pass hostname and port to app.listen()

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 
 const app = require("./app");
 
+const HOSTNAME = "127.0.0.1";
+const PORT = 3000;
 
-app.listen(3000, function () {
-  console.log("Listening on 3000");
+
+app.listen(PORT, HOSTNAME, function () {
+  console.log(`Listening on ${HOSTNAME}, port ${PORT}`);
 });
