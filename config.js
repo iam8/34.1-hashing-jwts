1c1b644e4700bf5f4faace4096679a74bc1a79e8
alexm <alex-m@outlook.com>
2023-05-03 12:11:32 -0700

    Add app hostname and port as constants and export them

--- config.js
+++ config.js
@@ -8,6 +8,9 @@
 
 require("dotenv").config();
 
+const HOSTNAME = "127.0.0.1";
+const PORT = 3000;
+
 const DB_URI = (process.env.NODE_ENV === "test")
     ? "postgresql:///messagely_test"
     : "postgresql:///messagely";
@@ -17,6 +20,8 @@ const BCRYPT_WORK_FACTOR = 12;
 
 
 module.exports = {
+    HOSTNAME,
+    PORT,
     DB_URI,
     SECRET_KEY,
     BCRYPT_WORK_FACTOR,
