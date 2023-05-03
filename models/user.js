7388b94a8ec2f35f9de51a1a725d7cc649ec5acb
alexm <alex-m@outlook.com>
2023-05-03 13:52:58 -0700

    Add working code to User.get()

--- models/user.js
+++ models/user.js
@@ -80,6 +80,18 @@ class User {
      *          last_login_at } */
     static async get(username) {
 
+        const qRes = await db.query(`
+            SELECT username, first_name, last_name, phone, join_at, last_login_at
+            FROM users
+            WHERE username = $1`,
+            [username]);
+
+        const user = qRes.rows[0];
+        if (!user) {
+            throw new ExpressError(`No such user: ${username}`, 404);
+        }
+
+        return user;
     }
 
     /** Return messages from this user.
